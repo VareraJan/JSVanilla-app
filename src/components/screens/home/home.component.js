c723c3d1099e85056bac440bb07d9e6c123dacf4
VareraJan <vareraj@gmail.com>
2023-08-10 07:08:19 +0300

    add BaseScreen class; start of core development

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,4 +1,10 @@
-export class Home {
+import { BaseScreen } from '@/core/components/base-screen.component'
+
+export class Home extends BaseScreen {
+	constructor() {
+		super({ title: 'Home' })
+	}
+
 	render() {
 		return '<p>Home</p>'
 	}
